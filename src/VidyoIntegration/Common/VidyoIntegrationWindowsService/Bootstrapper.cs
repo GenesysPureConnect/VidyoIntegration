4414de8b5d9a6e96c6a61410c837e86f211b5329
unknown <Tim.Smith@notamac.i3domain.inin.com>
2015-01-20 15:39:09 -0700

    Creating windows service

---
+++ src/VidyoIntegration/Common/VidyoIntegrationWindowsService/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using Newtonsoft.Json;
+
+namespace VidyoIntegrationTestConsole
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            container.Register<JsonSerializer, CustomJsonSerializer>();
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            //CORS Enable
+            pipelines.AfterRequest.AddItemToEndOfPipeline((ctx) =>
+            {
+                ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "POST,GET")
+                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+
+            });
+
+            base.RequestStartup(container, pipelines, context);
+        }
+    }
+}
